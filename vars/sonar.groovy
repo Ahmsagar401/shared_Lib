6fca105febb560f70ba106d8369c60d1d793143f
Ahmsagar401 <154943092+Ahmsagar401@users.noreply.github.com>
2024-01-22 23:27:28 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,6 +1,6 @@
 def call() {
        // Your SonarQube scan steps go here
-      // sh 'mvn sonar:sonar -Dsonar.projectKey=sonar -Dsonar.host.url=http://18.206.244.145:9000 -Dsonar.java.binaries=target/classes'
-         sh 'mvn sonar:sonar -Dsonar.projectKey=sonar -Dsonar.host.url=http://18.206.244.145:9000 -Dsonar=sonar.exclusions*/.java'
+      // sh 'mvn sonar:sonar -Dsonar.projectKey=sonar -Dsonar.host.url=http://18.170.23.153:9000 -Dsonar.java.binaries=target/classes'
+         sh 'mvn sonar:sonar -Dsonar.projectKey=sonar -Dsonar.host.url=http://18.170.23.153:9000 -Dsonar=sonar.exclusions*/.java'
    }
 return this
